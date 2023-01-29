e77c9a5b97f7985af3da9c1529153ffa59004186
Heiko Thiery <heiko.thiery@gmail.com>
2023-01-29 16:38:16 +0100

    app/database.py: rename database file
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///pkg-stats.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///br-pkg.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
